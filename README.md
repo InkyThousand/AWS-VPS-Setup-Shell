37ad0839a5d9cdf1c6c39ec9ff25d60e5d12f0c2
Pavel Losiev <inky.thousand@gmail.com>
2025-05-23 14:20:16 +0200

    Add describe for how to run script; Add subnets installation code

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ aws-vpc-setup
 ├── scripts
 │   └── vpc-setup.sh        # Bash script to set up the VPC
 └── README.md               # Project documentation
+```
 
 ## Prerequisites
 
@@ -20,3 +21,22 @@ aws-vpc-setup
 VPC: 10.0.0.0/24 (provides 256 IPs)
 Private Subnet: 10.0.0.0/26 (62 usable IPs)
 Public Subnet: 10.0.0.64/28 (14 usable IPs)
+
+## Running the Script
+
+To set up the VPC using the provided Bash script, follow these steps:
+
+1. Open a terminal and navigate to the `scripts` directory:
+   ```
+   cd aws-vpc-setup/scripts
+   ```
+
+2. Make the script executable:
+   ```
+   chmod +x vpc-setup.sh
+   ```
+
+3. Run the script:
+   ```
+   ./vpc-setup.sh
+   ```
--- scripts/vpc-setup.sh
+++ scripts/vpc-setup.sh
@@ -9,3 +9,32 @@ vpcid=$(aws ec2 create-vpc \
 
 echo "VPC created: $vpcid"
 
+# Create Public Subnet
+
+pubsub1=$(aws ec2 create-subnet \
+  --vpc-id $vpcid \
+  --cidr-block 10.0.0.64/28 \
+  --availability-zone us-west-2a \
+  --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Public Subnet}]' \
+  --query 'Subnet.SubnetId' \
+  --output text)
+
+echo "Public Subnet created: $pubsub1"
+
+# Enable Public IP on launch
+
+aws ec2 modify-subnet-attribute \
+  --subnet-id $pubsub1 \
+  --map-public-ip-on-launch
+
+# Create Private Subnet
+
+privsub1=$(aws ec2 create-subnet \
+  --vpc-id $vpcid \
+  --cidr-block 10.0.0.0/26 \
+  --availability-zone us-west-2a \
+  --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Private Subnet}]' \
+  --query 'Subnet.SubnetId' \
+  --output text)
+
+echo "Private Subnet created: $privsub1"
